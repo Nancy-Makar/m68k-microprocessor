da33987e9a81a3a17695c0d83cebd0274c1479dd
Nancy <you@example.com>
2023-03-20 23:37:28 -0700

    corrected error in address decoder

--- M68kV6.0 - 800by480/IIC_SPI_BUC_DECODER.v
+++ M68kV6.0 - 800by480/IIC_SPI_BUC_DECODER.v
@@ -11,8 +11,9 @@ output reg IIC0_Enable_H
 	
 	if(AS_L == 0 && IOSelect_H == 1)							
 		begin
-			// addresses in the range hex [0040 0000 - 0040 FFFF]
-			if(Address[15:4] == 12'b1000_0000_0010)
+			// the IOSelect_H signal comes from the IOSelect_H signal on the top level schematic which is asserted high for CPU
+			// addresses in the range hex [0040 0000 - 0040 FFFF] so you only need to decode the lower 16 address lines in conjunction with IOSelect_H
+			if(Address[15:4] == 12'b1000_0000_0000)
 				IIC0_Enable_H <= 1 ;
 
 		end
